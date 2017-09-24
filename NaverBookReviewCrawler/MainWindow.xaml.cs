6af4572f393f23c8f409a91a4998c29f0947e0ee
Kim Jong Feel <DeveloperFEEL@gmail.com>
2017-09-25 00:19:29 +0900

    first prototype

--- NaverBookReviewCrawler/MainWindow.xaml.cs
+++ NaverBookReviewCrawler/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,90 @@ namespace NaverBookReviewCrawler
         public MainWindow()
         {
             InitializeComponent();
+
+            // 네이버 북 네티즌 리뷰
+            // 1. 블로그 url
+            // 2. 게시 날짜
+            // 3. 글자 수
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Result_Author.Text = "";
+            Result_Review.Text = "";
+
+            bool IsContinue = true;
+            int page = 1;
+            int.TryParse(Page.Text, out page);
+            int pagebase = page;
+
+            string authors = "";
+            string contents = "";
+            int authorCount = 1;
+            int contentsCount = 1;
+
+            while (IsContinue)
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(URL.Text + "&&page=" + page);
+                page++;
+
+                HtmlDocument htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(html);
+
+                var htmlNodeCol = htmlDocument.DocumentNode.SelectNodes("//dd");
+
+                bool IsAuthorExist = false;
+                bool IsReviewTextExist = false;
+                foreach (var item in htmlNodeCol)
+                {
+                    if (item.Attributes["class"] != null && item.Attributes["class"].Value == "txt_inline")
+                    {
+                        bool dateTimeParseResult = DateTime.TryParse(item.InnerText, out DateTime result);
+                        if (dateTimeParseResult)
+                        {
+                            authors += result.ToShortDateString() + ", ";
+                        }
+                    }
+                    if (item.Id.Contains("review_author"))
+                    {
+                        string author = item.InnerText.Replace("\t", "").Replace("\r", "").Replace("\n", "");
+                        author = author.Insert(author.IndexOf("작성자명"), ", ");
+                        int index = author.IndexOf("블로그명");
+                        if (index != -1)
+                        {
+                            author = author.Insert(index, ", ");
+                        }                        
+                        authors += author + "\r\n";
+
+                        IsAuthorExist = true;
+                    }
+                    
+                    if (item.Id.Contains("review_text_"))
+                    {
+                        string replaced = item.InnerText.Replace('\n', ' ');
+                        contents += "Review " + contentsCount++ + "----------------------------";
+                        contents += replaced + "\r\n";
+
+                        IsReviewTextExist = true;
+                    }
+                }
+
+                if (IsAuthorExist == false && IsReviewTextExist == false)
+                {
+                    IsContinue = false;
+                }
+
+                if (pagebase + 10 == page)
+                {
+                    IsContinue = false;
+                }
+            }
+
+            Result_Author.Text = "작성날짜, 블로그주소, 작성자명, 블로그명" + "\r\n";
+            Result_Author.Text += authors + "\r\n\r\n\r\n";
+            Result_Review.Text += contents;
         }
     }
 }
